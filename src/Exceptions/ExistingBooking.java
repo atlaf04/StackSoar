5d24fe7d65b134211479d21f09dbba3398951d80
atlaf04 <143972478+atlaf04@users.noreply.github.com>
2023-12-18 15:07:17 -0500

    Update ExistingBooking.java
    existing booking

--- src/Exceptions/ExistingBooking.java
+++ src/Exceptions/ExistingBooking.java
@@ -1,5 +1,17 @@
 package Exceptions;
 
-public class ExistingBooking {
+// handling existing bookings
+//extends the base exception class, making it a checked exception
 
+public class ExistingBooking extends Exception {
+ 
+
+// constructor takes a message parameter for a descriptive message
+ // using 'super(message)' to invoke the base exception class constructor
+ public ExistingBooking(String message) {
+     super(message);
+ }
+ 
+ // ready to be used to indicate an existing booking situation
 }
+
