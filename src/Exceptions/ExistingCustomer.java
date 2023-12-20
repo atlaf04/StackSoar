54534f694fe2aba6138423dd020c8acc2c5d9112
atlaf04 <143972478+atlaf04@users.noreply.github.com>
2023-12-19 21:33:46 -0500

    existing customer exception
    existing customer exception

--- src/Exceptions/ExistingCustomer.java
+++ src/Exceptions/ExistingCustomer.java
@@ -1,5 +1,8 @@
 package Exceptions;
 
-public class ExistingCustomer {
-
+public class ExistingCustomer extends Exception{
+	public ExistingCustomer(String s) {
+		super(s);
+	}
+// will this push
 }
