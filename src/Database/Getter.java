9ecb091c273ba4a6b6fb7c0e7e7ff37b90696ca1
EduardoSantos02 <144863416+EduardoSantos02@users.noreply.github.com>
2023-12-23 15:49:16 -0500

    forgot to save
    all the code came through

--- src/Database/Getter.java
+++ src/Database/Getter.java
@@ -1,5 +1,220 @@
 package Database;
 
+import java.sql.Connection;
+
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import project.Customer;
+import project.Flights;
+import project.FlightReservation;
+import java.util.ArrayList;
+import java.util.List;
 public class Getter {
+
+	static final String db_url = "jdbc:mysql://127.0.0.1:3306/?user=root";
+	static final String db_username = "root";
+	static final String db_pw = "August13";
+
+	public static Customer getCustomer(String email) throws Exception {
+		Customer cust = new Customer();
+		try {
+			Connection con = getConnection();
+			PreparedStatement check = con.prepareStatement("Select COUNT(*) from Customer_Table where CustomerEmail = '" + email + "'");
+			ResultSet exist = check.executeQuery();
+			exist.next(); 
+			if (exist.getInt(1) == 0) {//checking if customer exists in the database
+				System.out.println("Customer does not exist");
+            }
+            else {
+
+			PreparedStatement statement = con.prepareStatement("Select * from Customer_Table where CustomerEmail = '" + email + "'");
+			ResultSet result = statement.executeQuery();
+			while (result.next()) { // try getting the customer information and set
+				
+				cust.setEmail(result.getString(1));
+				cust.setPassword(result.getString(2));
+				cust.setFirstname(result.getString(3));
+				cust.setLastname(result.getString(4));
+				cust.setAddress(result.getString(5));
+				cust.setState(result.getString(6));
+				cust.setZip(result.getInt(7));
+				cust.setSsn(result.getInt(8));
+				cust.setSecurityanswer(result.getString(9));
+}
+			statement.close();
+			result.close();
+            }
+            
+		} catch (Exception e) {
+			System.out.println("Could not find the customer information in the system. Please try again.");
+		}
+		return cust; // return the customer that has all the informations
+        
+    }
+
+	
+	public static Flights getFlight(int id) throws Exception { // get flight using flightid
+		Flights flight = new Flights(); 
+		try { 
+			Connection con = getConnection();
+			PreparedStatement statement = con.prepareStatement("Select * from Flight_Table where FlightID = '" + id + "'");
+			ResultSet result = statement.executeQuery();
+			while (result.next()) {
+				flight.setFlightid(result.getInt("FlightID"));
+				flight.setDeparturedate(result.getString("DepartureDate"));
+				flight.setDeparturetime(result.getString("DepartureTime"));
+				flight.setArrivaldate(result.getString("ArrivalDate"));
+				flight.setArrivaltime(result.getString("ArrivalTime"));
+				flight.setOriginalairport(result.getString("Origin"));
+				flight.setDestinationairport(result.getString("Destination"));
+				flight.setCapacity(result.getInt("Capacity"));
+				flight.setSeatsavailable(result.getInt("SeatsAvailable"));
+				
+			}
+			statement.close();
+			result.close();
+		}
+		
+		catch (Exception ex) {
+			System.out.println("Could not find flight information in the system. Please try again.");
+		}
+		return flight;
+    }
+	public static Flights getFlight(String origin, String destination, String departuredate, String arrivaldate) throws Exception { //get flight using origin, destination, departuredate, and arrivaldate
+		Flights flight = new Flights(); 
+		try { 
+			Connection con = getConnection();
+			PreparedStatement statement = con.prepareStatement("Select * from Flight_Table where Origin = '" + origin + "' and Destination = '" + destination +"' and DepartureDate = '"+ departuredate + "' and ArrivalDate = '"+ arrivaldate + "';");
+			ResultSet result = statement.executeQuery();
+			while (result.next()) {
+				flight.setFlightid(result.getInt("FlightID"));
+				flight.setDeparturedate(result.getString("DepartureDate"));
+				flight.setDeparturetime(result.getString("DepartureTime"));
+				flight.setArrivaldate(result.getString("ArrivalDate"));
+				flight.setArrivaltime(result.getString("ArrivalTime"));
+				flight.setOriginalairport(result.getString("Origin"));
+				flight.setDestinationairport(result.getString("Destination"));
+				flight.setCapacity(result.getInt("Capacity"));
+				flight.setSeatsavailable(result.getInt("SeatsAvailable"));
+
+			}
+			statement.close();
+			result.close();
+
+		}
+		catch (Exception ex) {
+			System.out.println("Could not find flight information in the system. Please try again.");
+		}
+		return flight;
+    }
+
+	public static FlightReservation getReservation(int reservationid, String customeremeail) throws Exception {//get reservation using reservation id and customeremail
+		FlightReservation reservation = new FlightReservation();
+		try {
+			Connection con = getConnection();
+			PreparedStatement statement = con.prepareStatement("Select * from Reservation_T where ReservationId = '" + reservationid+ "' and CustomerEmail = '"+ customeremeail + "'");
+			ResultSet result = statement.executeQuery();
+			while (result.next()) {
+				reservation.setReservationId(result.getInt("ReservationID"));
+				reservation.setCustomerEmail(result.getString("CustomerEmail"));;
+				reservation.setFlightId(result.getInt("FlightID"));
+			}
+			statement.close();
+			result.close();
+
+		} catch (Exception ex) {
+			System.out.println("Can't get reservation");
+		}
+		return reservation;
+    
+    }
+	public static ArrayList<FlightReservation> getReservations(String customerEmail) throws Exception {// get lists of reservations of a customer
+		ArrayList<FlightReservation> reservations = new ArrayList<>();
+		try {
+			Connection con = getConnection();
+			PreparedStatement statement = con.prepareStatement("SELECT * FROM Reservation_T WHERE CustomerEmail = ?");
+
+			statement.setString(1, customerEmail);
+
+			ResultSet result = statement.executeQuery();
+
+			while (result.next()) {
+				FlightReservation reservation = new FlightReservation();
+				reservation.setReservationId(result.getInt("ReservationID"));
+				reservation.setCustomerEmail(result.getString("CustomerEmail"));
+				reservation.setFlightId(result.getInt("FlightID"));
+
+					reservations.add(reservation);
+            }
+        } catch (Exception ex) {
+			System.out.println("Error retrieving reservation information: " + ex.getMessage());
+		}
+
+		return reservations;
+    }
+
+	public static ArrayList<Flights> getFlights(String origin, String destination, String departureDate, String arrivalDate) throws Exception { // get lists of flight
+		ArrayList<Flights> flights = new ArrayList<>();
+		try {
+			Connection con = getConnection();
+			PreparedStatement statement = con.prepareStatement("SELECT * FROM Flight_T WHERE Origin = ? AND Destination = ? AND DepartureDate = ? AND ArrivalDate = ?");
+			statement.setString(1, origin);
+			statement.setString(2, destination);
+			statement.setString(3, departureDate);
+			statement.setString(4, arrivalDate);
+
+			ResultSet result = statement.executeQuery();
+
+			while (result.next()) {
+				Flights flight = new Flights();
+				flight.setFlightid(result.getInt("FlightID"));
+				flight.setDeparturedate(result.getString("DepartureDate"));
+				flight.setDeparturetime(result.getString("DepartureTime"));
+				flight.setArrivaldate(result.getString("ArrivalDate"));
+				flight.setArrivaltime(result.getString("ArrivalTime"));
+				flight.setOriginalairport(result.getString("Origin"));
+				flight.setDestinationairport(result.getString("Destination"));
+				flight.setCapacity(result.getInt("Capacity"));
+				flight.setSeatsavailable(result.getInt("SeatsAvailable"));
+				
+				
+				flights.add(flight);
+            }
+        } 
+			catch (SQLException e) {
+				e.printStackTrace();
+        }
+        catch (Exception ex) {
+			System.out.println("Error retrieving flight information: " + ex.getMessage());
+		} 
+
+		return flights;
+    }
+
+	public static ArrayList<Flights> getAllFlightsfromAccount(String email) throws Exception { // get all flights from one account
+		ArrayList<Flights> flights = new ArrayList<Flights>();
+		ArrayList<FlightReservation> reservations = Getter.getReservations(email); //first get the list of reservations
+		for (int i = 0; i< reservations.size();i++) {
+			flights.add(Getter.getFlight(reservations.get(i).getFlightId())); // then go through that list of reservations, get the flight id, then use the flight id to get the flights -> then add the flights into the array
+		}
+		return flights;
+        
+    }
+	
+	public static Connection getConnection() throws Exception {
+		try {
+			String driver = "com.mysql.cj.jdbc.Driver";
+			Class.forName(driver);
+			Connection conn = DriverManager.getConnection(db_url,db_username,db_pw);
+			return conn;
+		}
+		catch (Exception ex) {
+			System.out.println(ex);
+		}
+		return null;
+    }
 
 }
--- src/Database/StackSoarDatabase.java
+++ src/Database/StackSoarDatabase.java
@@ -1,5 +1,165 @@
 package Database;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+//import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class StackSoarDatabase {
+    // Main method
+    public static void main(String[] args) {
+        try {
+            // Call the 'get' method to retrieve data
+            get();
+        } catch (Exception e) {
+            System.out.println("Select failed");
+        }
+    }
 
+    // Method to create the Customer_T table
+    public static void createCustomerTable() throws Exception {
+        try {
+            Connection con = getConnection();
+            PreparedStatement create = con.prepareStatement("CREATE TABLE IF NOT EXISTS Customer_T(" +
+                    "CustomerEmail VARCHAR(100) NOT NULL," +
+                    "CustomerPassword VARCHAR(30)," +
+                    "CustomerFName VARCHAR(25)," +
+                    "CustomerLName VARCHAR(25)," +
+                    "CustomerAddress VARCHAR(30)," +
+                    "CustomerState CHAR(2)," +
+                    "CustomerZip VARCHAR(20)," +
+                    "CustomerSSN VARCHAR(9)," +
+                    "CustomerSecurityAnswer VARCHAR(100)," +
+                    "PRIMARY KEY(CustomerEmail));");
+            create.executeUpdate();
+            create.close();
+        } catch (Exception ex) {
+            System.out.println(ex);
+        } finally {
+            System.out.println("Function createCustomerTable completed");
+            getConnection().close();
+        }
+    }
+
+    // Method to create the Flight_T table
+    public static void createFlightTable() throws Exception {
+        try {
+            Connection con = getConnection();
+            PreparedStatement create = con.prepareStatement("CREATE TABLE IF NOT EXISTS Flight_T(" +
+                    "FlightID VARCHAR(6) NOT NULL," +
+                    "DepartureDate DATE," +
+                    "DepartureTime TIME," +
+                    "ArrivalDate DATE," +
+                    "ArrivalTime TIME," +
+                    "Origin VARCHAR(35)," +
+                    "Destination VARCHAR(35)," +
+                    "Capacity INT," +
+                    "PRIMARY KEY(FlightId));");
+            create.executeUpdate();
+            create.close();
+        } catch (Exception ex) {
+            System.out.println(ex);
+        } finally {
+            System.out.println("Function createFlightTable completed");
+            getConnection().close();
+        }
+    }
+
+    // Method to insert values into the Customer_T table
+    public static void post() throws Exception {
+        final String first_test = "John";
+        final String last_test = "Miller";
+        final String email_test = "johnmiller@gmail.com";
+        final String username_test = "johnmiller";
+        final String password_test = "password";
+        final String address_test = "123 Street";
+        final String state_test = "GA";
+        final String zip_test = "12345";
+        final String ssn_test = "123456789";
+        final String question_test = "Hi";
+        final String answer_test = "Bye";
+
+        try {
+            Connection con = getConnection();
+            PreparedStatement posted = con.prepareStatement("INSERT INTO Customer_T VALUES " +
+                    "('" + email_test + "','" + username_test + "','" + password_test + "','" + first_test + "','" +
+                    last_test + "','" + address_test + "','" + state_test + "','" + zip_test + "','" + ssn_test + "','" +
+                    question_test + "','" + answer_test + "')");
+            posted.executeUpdate();
+            posted.close();
+
+        } catch (Exception ex) {
+            System.out.println("Insert unsuccessfully");
+        } finally {
+            System.out.println("Insert completed");
+            getConnection().close();
+        }
+    }
+
+    // Method to retrieve data from the Customer_T table
+    public static ArrayList<String> get() throws Exception {
+        try {
+            Connection con = getConnection();
+            PreparedStatement statement = con.prepareStatement("SELECT * FROM Customer_T");
+            ResultSet result = statement.executeQuery();
+            ArrayList<String> list = new ArrayList<>();
+            while (result.next()) {
+                // Output each field for the selected record
+                System.out.println(result.getString("CustomerEmail"));
+                System.out.println(result.getString("CustomerUsername"));
+                System.out.println(result.getString("CustomerPassword"));
+                System.out.println(result.getString("CustomerFName"));
+                System.out.println(result.getString("CustomerLName"));
+                System.out.println(result.getString("CustomerAddress"));
+                System.out.println(result.getString("CustomerState"));
+                System.out.println(result.getString("CustomerZip"));
+                System.out.println(result.getString("CustomerSSN"));
+                System.out.println(result.getString("CustomerSecurityQuestion"));
+                System.out.println(result.getString("CustomerSecurityAnswer"));
+
+                // Add each field to the list
+                list.add(result.getString("CustomerEmail"));
+                list.add(result.getString("CustomerUsername"));
+                list.add(result.getString("CustomerPassword"));
+                list.add(result.getString("CustomerFName"));
+                list.add(result.getString("CustomerLName"));
+                list.add(result.getString("CustomerAddress"));
+                list.add(result.getString("CustomerState"));
+                list.add(result.getString("CustomerZip"));
+                list.add(result.getString("CustomerSSN"));
+                list.add(result.getString("CustomerSecurityQuestion"));
+                list.add(result.getString("CustomerSecurityAnswer"));
+            }
+            System.out.println("ALL records have been selected");
+            statement.close();
+            result.close();
+            return list;
+
+        } catch (Exception ex) {
+            System.out.println(ex);
+        } finally {
+            getConnection().close();
+        }
+        return null;
+    }
+
+    // Method to establish a database connection
+    public static Connection getConnection() throws Exception {
+        Scanner input = new Scanner(System.in);
+        try {
+            String driver = "com.mysql.cj.jdbc.Driver";
+            String url = "jdbc:mysql://127.0.0.1:3306/?user=root"; // Enter YOUR url for localhost
+            String username = "root";
+            String password = "August13";
+            Class.forName(driver);
+            Connection conn = DriverManager.getConnection(url, username, password);
+            System.out.println("Connected");
+            return conn;
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+        return null;
+    }
 }
