f30c688ee353f186d3dd8ae75991cad4b9e22365
atlaf04 <143972478+atlaf04@users.noreply.github.com>
2023-12-27 11:39:57 -0500

    getter w notes

--- src/Database/Getter.java
+++ src/Database/Getter.java
@@ -11,15 +11,14 @@ import project.Customer;
 import project.Flights;
 import project.FlightReservation;
 import java.util.ArrayList;
-import java.util.List;
 public class Getter {
 
-	static final String db_url = "jdbc:mysql://127.0.0.1:3306/StackSoar";
+	static final String db_url = "jdbc:mysql://127.0.0.1:3306/StackSoar"; // connecting to the database 
 	static final String db_username = "root";
 	static final String db_pw = "August13";
 
 	public static Customer getCustomer(String email) throws Exception {
-		Customer cust = new Customer();
+		Customer cust = new Customer(); // cust is an object of the customer class
 		try {
 			Connection con = getConnection();
 			PreparedStatement check = con.prepareStatement("Select COUNT(*) from Customer_Table where CustomerEmail = '" + email + "'");
@@ -209,18 +208,22 @@ public class Getter {
 		
 	}
 	
-	public static Connection getConnection() throws Exception {
+	public static Connection getConnection() throws Exception { // this is wh
 	
 		try {
 			String driver = "com.mysql.cj.jdbc.Driver";
-			Class.forName(driver);
-			Connection conn = DriverManager.getConnection(db_url,db_username,db_pw);
+			
+			Class.forName(driver); // forName is a method that is part of the java.lang.Class
+			// 
+			Connection conn = DriverManager.getConnection(db_url,db_username,db_pw); 
+	        // Establishes a connection to the MySQL database using the DriverManager class.
+	        // Takes three parameters: the database URL, the username, and the password.
 			return conn;
 		}
 		catch (Exception ex) {
-			System.out.println(ex);
+			System.out.println(ex); // Prints exception details to the console if an exception occurs during the connection process.
 		}
-		return null;
+		return null;  // Returns null if the connection could not be established. This helps prevent a null pointer exception.
 	}
 
 } // changed 
